ac2023b994e6ecfafd13d68cffc89084b8eaa3ff
mic050r <103114387+mic050r@users.noreply.github.com>
2023-12-31 21:56:44 +0900

    feat : 할 일을 데이터로

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -4,6 +4,10 @@ import "./index.js";
 import Todo from "./components/Todo"; // todo 불러오기
 
 function App(props) {
+  const taskList = props.tasks.map((task) => (
+    <Todo id={task.id} name={task.name} completed={task.completed} />
+  ));
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -46,9 +50,7 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        <Todo name="Eat" completed={true} id="todo-0" />
-        <Todo name="Sleep" completed={false} id="todo-1" />
-        <Todo name="Repeat" completed={false} id="todo-2" />
+        {taskList}
       </ul>
     </div>
   );
