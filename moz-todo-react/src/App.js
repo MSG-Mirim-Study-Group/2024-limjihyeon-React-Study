0f51e9bef4683d9074f8e2774fd434c963c8af83
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-02 15:50:43 +0900

    feat : 작업 완료 기능 추가(체크박스 체크)

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -7,12 +7,30 @@ import { nanoid } from "nanoid";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
+  function toggleTaskCompleted(id) {
+    // tasks 배열을 map 함수를 사용하여 새로운 배열로 업데이트
+    const updatedTasks = tasks.map((task) => {
+      // 만약 현재 작업의 ID가 편집된 작업의 ID와 같다면
+      if (id === task.id) {
+        // 새로운 객체를 만들기 위해 객체 전개 연산자를 사용하여
+        // `completed` 속성을 반전시킨 새로운 객체 반환
+        // console.log({ ...task, completed: !task.completed });
+        return { ...task, completed: !task.completed };
+      }
+      // 편집되지 않은 작업은 그대로 반환
+      return task;
+    });
+    // 업데이트된 작업 배열을 상태로 설정
+    setTasks(updatedTasks);
+  }
+
   const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
 
