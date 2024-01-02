135cb96ca84153ead59784efbed335207cbfaae5
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-02 16:25:27 +0900

    feat : 사용자가 할 일을 삭제 할 때 제목 포커싱하기

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -12,6 +20,7 @@ const FILTER_MAP = {
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
+  const listHeadingRef = useRef(null);
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState("All");
   const filterList = FILTER_NAMES.map((name) => (
@@ -92,13 +101,23 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
 
+  const prevTaskLength = usePrevious(tasks.length);
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic for 임지현</h1>
       <Form addTask={addTask} />
 
       <div className="filters btn-group stack-exception">{filterList}</div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
