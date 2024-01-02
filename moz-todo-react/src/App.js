8d00a3f9fa26747229c52c3ee5172fba52cccb21
mic050r <103114387+mic050r@users.noreply.github.com>
2024-01-02 16:05:09 +0900

    feat : 체크한 한 작업과 체크 안 한 작업 필터링으로 구분

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -4,8 +4,37 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  function FilterButton(props) {
+    return (
+      <button
+        type="button"
+        className="btn toggle-btn"
+        aria-pressed={props.isPressed}
+        onClick={() => props.setFilter(props.name)}>
+        <span className="visually-hidden">보여주다</span>
+        <span>{props.name}</span>
+        <span className="visually-hidden">할 일들</span>
+      </button>
+    );
+  }
 
   function toggleTaskCompleted(id) {
     // tasks 배열을 map 함수를 사용하여 새로운 배열로 업데이트
@@ -41,17 +70,19 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const taskList = tasks.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  ));
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
 
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -66,11 +97,7 @@ function App(props) {
       <h1>TodoMatic for 임지현</h1>
       <Form addTask={addTask} />
 
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
